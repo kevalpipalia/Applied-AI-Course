55d529136a40d4dc817a3c3456e21a13450540f8
Keval Pipalia <keval.pipalia009@yahoo.com>
2020-03-16 02:19:32 +0530

    created program to common elements in two lists using dictionary
    Python Dictionary provides instant lookups so it can search for particular elemnt in O(1) unit of time
    
    Time Complexity: O(m+n)
    Space Complexity: O(m)
    
    It is faster than sequential algorithm but requires more RAM.

---
+++ Common in lists - Dictionary/commondict.py
@@ -0,0 +1,17 @@
+import random
+
+l1 = list(range(100))
+random.shuffle(l1)
+
+l2 = list(range(50))
+random.shuffle(l2)
+
+smallList = {}
+for ele in l2:
+    smallList[ele] = 1;
+count = 0;
+for i in l1:
+    if smallList.get(i) != None:
+        print(i)
+        count+=1;
+print("No of common elements:", count)
